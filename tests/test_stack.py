77e62548d5d7bdac3a58e75540508e4ef2f1c01e
Andrew_Polik <polik.24@mail.ru>
2023-11-13 02:17:49 +0300

    Реализация класса Node, реализация конструктора и метода push класса Stack. Написан тест

--- tests/test_stack.py
+++ tests/test_stack.py
@@ -1,1 +1,13 @@
+import unittest
+from src.stack import Stack
+
 """Здесь надо написать тесты с использованием unittest для модуля stack."""
+
+
+class TestStack(unittest.TestCase):
+    def setUp(self):
+        self.stack = Stack()
+
+    def test_push(self):
+        self.assertEqual(self.stack.push('data1'), None)
+        self.assertIsNone(self.stack.push('data1'))
